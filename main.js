c5e497debbf18ec754e4f70d325b8e84eb423d6f
Andreas Olsson <andreasolsson660@gmail.com>
2020-03-01 18:55:14 +0100

    changing js code

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
-let menuLists = document.getElementsByClassName('menu-list');
-    let menuLabels = document.getElementsByClassName('menu-label');
-    let toggleMenuBtn = document.getElementById('toggleNav');
-    let linkTabs = document.getElementsByClassName('linkTabs');
-    for ( let linkTab of linkTabs ) {
-        linkTab.hidden = true;
+let menuLists = document.querySelectorAll(".menu-list");
+let menuLabels = document.querySelectorAll(".menu-label");
+let toggleMenuBtn = document.getElementById("toggleNav");
+let linkTabs = document.querySelectorAll(".linkTabs");
+
+for (let linkTab of linkTabs) {
+  linkTab.hidden = true;
+}
+
+toggleMenuBtn.addEventListener("click", () => {
+  document.getElementById("showTab").classList.toggle("show");
+});
+for (let menuList of menuLists) {
+  menuList.hidden = true;
+}
+
+for (let menuLabel of menuLabels) {
+  menuLabel.addEventListener("click", event => {
+    for (let menuLabel of menuLabels) {
+      menuLabel.classList.remove("active");
     }
-
-    toggleMenuBtn.addEventListener('click', () => {
-        document.getElementById('test').classList.toggle('show');
-    });
-    for ( let menuList of menuLists ) {
-        menuList.hidden = true;
+    for (let menuList of menuLists) {
+      menuList.hidden = true;
     }
+    event.target.nextElementSibling.hidden = false;
 
-    for ( let menuLabel of menuLabels ) {
-        menuLabel.addEventListener('click', (event) => {
-            for ( let menuLabel of menuLabels )   {
-                menuLabel.classList.remove('active');
-            }
-            for ( let menuList of menuLists ) {
-                menuList.hidden = true;
-            }
-            event.target.nextElementSibling.hidden = false;
+    event.target.classList.add("active");
+  });
+}
 
-            event.target.classList.add('active');
-        });
+function showTab(event, category) {
+  for (let linkTab of linkTabs) {
+    linkTab.hidden = true;
+  }
+  document.getElementById(category).hidden = false;
+}
+
+addEventListener("click", () => {
+  if (!event.target.matches(".linkTabs, .nav-item, .linkTabs a, .linkTabs p")) {
+    for (let linkTab of linkTabs) {
+      linkTab.hidden = true;
     }
+  }
+});
 
+window.sr = ScrollReveal();
 
-
-    function test(event, category) {
-        for ( let linkTab of linkTabs ) {
-            linkTab.hidden = true;
-        }
-        document.getElementById(category).hidden = false;
-    }
-
-    addEventListener('click', () => {
-        if (!event.target.matches('.linkTabs, .nav-item, .linkTabs a, .linkTabs p')) {
-            console.log('fff');
-            for ( let linkTab of linkTabs ) {
-                linkTab.hidden = true;
-            }
-        }
-    });
-
-    window.sr = ScrollReveal();
-
-    sr.reveal('.info .animate', { duration: 2000,origin:'bottom', distance: '300px' }, 550);
+sr.reveal(
+  ".info .animate",
+  { duration: 2000, origin: "bottom", distance: "300px" },
+  550
+);
